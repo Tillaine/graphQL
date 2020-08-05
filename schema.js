7c45fd34f1e4981e627ef9ad8db15e58b069fa49
Tillaine <55994239+Tillaine@users.noreply.github.com>
2020-08-04 23:39:13 -0700

    adding comments

--- schema.js
+++ schema.js
@@ -1,17 +1,26 @@
-const { users }  = require("./data.js")
+const { users }  = require("./data.js");
+const axios = require("axios");
 const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
     GraphQLList,
-    GraphQLNonNull
+    GraphQLNonNull,
 } = require('graphql');
+const newComments = [];
 
 
 // Root Query
 
-
+const commentType = new GraphQLObjectType({
+    name: "Comment",
+    fields: () => ({
+        id: {type: GraphQLInt},
+        body: {type: GraphQLString},
+        name: {type: GraphQLString},
+    })
+})
 
 const userType = new GraphQLObjectType({
     name: "User", 
@@ -34,18 +43,59 @@ const RootQuery = new GraphQLObjectType({
                 id:{type: GraphQLInt}
             }, 
             resolve(parentValue, args){
-                for (let i = 0; i < users.length; i++) {
-                   if (users[i].id === args.id) {
-                       return users[i]
-                   }
-                }
+                return axios.get(`https://jsonplaceholder.typicode.com/users/${args.id}` )
+                .then(res => {
+                    console.log(res.data)
+                    return res.data
+                })
+            }
+        },
+        users: {
+            type: new GraphQLList(userType), 
+            resolve(parentVaules, args){
+                return axios.get(`https://jsonplaceholder.typicode.com/users/` )
+                .then(res => {
+                    return res.data
+                })
+            }
+        }, 
+        comments: {
+            type: new GraphQLList(commentType),
+            resolve(parentValue, args) {
+                return axios.get(`https://jsonplaceholder.typicode.com/comments/` )
+                .then(res => {
+                    return res.data
+                })
             }
         }
     }
 });
 
-
+const mutation = new GraphQLObjectType({
+    name: 'Mutation', 
+    fields: {
+        addUser: {
+            type: userType,
+            args:{
+                id: {type: new GraphQLNonNull(GraphQLInt)},
+                body: {type: new GraphQLNonNull(GraphQLString)},
+                name: {type: new GraphQLNonNull(GraphQLString)}
+            },
+            resolve(parentValue, args){
+                commentObj = {
+                    id: args.id,
+                    body: args.body,
+                    name: args.name
+                }
+                newComments.push(commentObj)
+                console.log(newComments)
+                return newComments
+            }
+        }
+    }
+})
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery, 
+    mutation
 });
